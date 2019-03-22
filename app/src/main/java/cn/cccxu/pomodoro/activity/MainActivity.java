0bd39c1adf418431a51855d6a4f494de6324ae8b
cccxu <cccxu@outlook.com>
2019-03-22 19:11:12 +0800

    Initial commit already has todo list but still some bug to handle

---
+++ app/src/main/java/cn/cccxu/pomodoro/activity/MainActivity.java
@@ -0,0 +1,139 @@
+package cn.cccxu.pomodoro.activity;
+
+import android.app.FragmentTransaction;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import cn.cccxu.pomodoro.R;
+import cn.cccxu.pomodoro.fragment.HistoryFragment;
+import cn.cccxu.pomodoro.fragment.MusicFragment;
+import cn.cccxu.pomodoro.fragment.SettingsFragment;
+import cn.cccxu.pomodoro.fragment.StartFragment;
+import cn.cccxu.pomodoro.fragment.TodoFragment;
+
+public class MainActivity extends AppCompatActivity {
+    private TodoFragment todoFragment;
+    private MusicFragment musicFragment;
+    private StartFragment startFragment;
+    private HistoryFragment historyFragment;
+    private SettingsFragment settingsFragment;
+
+    private BottomNavigationViewEx.OnNavigationItemSelectedListener mNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()){
+                case R.id.todo:
+                    showFragment(R.id.todo_fragment);
+                    return true;
+                case R.id.music:
+                    showFragment(R.id.music_fragment);
+                    return true;
+                case R.id.start:
+                    showFragment(R.id.start_fragment);
+                    return true;
+                case R.id.settings:
+                    showFragment(R.id.settings_fragment);
+                    return true;
+                case R.id.history:
+                    showFragment(R.id.history_fragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Promodoro");
+
+        BottomNavigationViewEx bnve = (BottomNavigationViewEx) findViewById(R.id.bnve);
+        bnve.setOnNavigationItemSelectedListener(mNavigationItemSelectedListener);
+
+
+        //fragment init and bind
+        todoFragment = new TodoFragment();
+        musicFragment = new MusicFragment();
+        startFragment = new StartFragment();
+        historyFragment = new HistoryFragment();
+        settingsFragment = new SettingsFragment();
+
+        FragmentTransaction beginTransaction = getFragmentManager().beginTransaction();
+        beginTransaction.add(R.id.content, todoFragment)
+                .add(R.id.content, musicFragment)
+                .add(R.id.content, startFragment)
+                .add(R.id.content, historyFragment)
+                .add(R.id.content, settingsFragment)
+                .commit();
+
+        hideAll();
+
+        showFragment(R.id.todo_fragment);
+    }
+
+    private void hideAll(){
+        FragmentTransaction beginTransaction = getFragmentManager().beginTransaction();
+        beginTransaction.hide(todoFragment)
+                .hide(musicFragment)
+                .hide(startFragment)
+                .hide(historyFragment)
+                .hide(settingsFragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    private void showFragment(int frag){
+        BottomNavigationViewEx bnve = (BottomNavigationViewEx) findViewById(R.id.bnve);
+        FragmentTransaction beginTransaction = getFragmentManager().beginTransaction();
+        switch(frag){
+            case R.id.todo_fragment:
+                hideAll();
+                beginTransaction.show(todoFragment);
+                beginTransaction.addToBackStack(null);
+                beginTransaction.commit();
+                bnve.setBackgroundColor(Color.parseColor("#0071C9"));
+                break;
+            case R.id.music_fragment:
+                hideAll();
+                beginTransaction.show(musicFragment);
+                beginTransaction.addToBackStack(null);
+                beginTransaction.commit();
+                break;
+            case R.id.start_fragment:
+                hideAll();
+                beginTransaction.show(startFragment);
+                beginTransaction.addToBackStack(null);
+                beginTransaction.commit();
+                break;
+            case R.id.history_fragment:
+                hideAll();
+                beginTransaction.show(historyFragment);
+                beginTransaction.addToBackStack(null);
+                beginTransaction.commit();
+                break;
+            case R.id.settings_fragment:
+                hideAll();
+                beginTransaction.show(settingsFragment);
+                beginTransaction.addToBackStack(null);
+                beginTransaction.commit();
+                break;
+
+        }
+    }
+}
---
+++ app/src/main/java/cn/cccxu/pomodoro/fragment/TodoFragment.java
@@ -0,0 +1,105 @@
+package cn.cccxu.pomodoro.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.yalantis.beamazingtoday.interfaces.AnimationType;
+import com.yalantis.beamazingtoday.interfaces.BatModel;
+import com.yalantis.beamazingtoday.listeners.BatListener;
+import com.yalantis.beamazingtoday.listeners.OnItemClickListener;
+import com.yalantis.beamazingtoday.listeners.OnOutsideClickedListener;
+import com.yalantis.beamazingtoday.ui.adapter.BatAdapter;
+import com.yalantis.beamazingtoday.ui.animator.BatItemAnimator;
+import com.yalantis.beamazingtoday.ui.callback.BatCallback;
+import com.yalantis.beamazingtoday.ui.widget.BatRecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.cccxu.pomodoro.R;
+import cn.cccxu.pomodoro.model.Goal;
+
+public class TodoFragment extends Fragment implements BatListener, OnItemClickListener, OnOutsideClickedListener {
+
+    private BatRecyclerView mRecyclerView;
+    private BatAdapter mAdapter;
+    private List<BatModel> mGoals;
+    private BatItemAnimator mAnimator;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState){
+        return inflater.inflate(R.layout.todo, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View container, @Nullable Bundle savedInstanceState)
+    {
+        mRecyclerView = (BatRecyclerView) getView().findViewById(R.id.todo_list);
+        mAnimator = new BatItemAnimator();
+
+        mRecyclerView.getView().setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        mRecyclerView.getView().setAdapter(mAdapter = new BatAdapter(mGoals = new ArrayList<BatModel>(){{}}, this, mAnimator)
+                .setOnItemClickListener(this)
+                .setOnOutsideClickListener(this));
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new BatCallback(this));
+        itemTouchHelper.attachToRecyclerView(mRecyclerView.getView());
+        mRecyclerView.getView().setItemAnimator(mAnimator);
+        mRecyclerView.setAddItemListener(this);
+
+        getView().findViewById(R.id.todo_fragment).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mRecyclerView.revertAnimation();
+            }
+        });
+    }
+
+    @Override
+    public void add(String s) {
+        mGoals.add(0, new Goal(s));
+        mAdapter.notify(AnimationType.ADD, 0);
+    }
+
+    @Override
+    public void delete(int i) {
+        mGoals.remove(i);
+        mAdapter.notify(AnimationType.REMOVE, i);
+    }
+
+    @Override
+    public void move(int from, int to) {
+        if(from >= 0 && to >= 0){
+            mAnimator.setPosition(to);
+            BatModel model = mGoals.get(from);
+            mGoals.remove(from);
+            mGoals.add(to, model);
+            mAdapter.notify(AnimationType.MOVE, from, to);
+        }
+
+        if(from ==0 || to == 0){
+            mRecyclerView.getView().scrollToPosition(Math.min(from, to));
+        }
+    }
+
+    @Override
+    public void onClick(BatModel item, int position){
+        Toast.makeText(this.getActivity(), item.getText(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onOutsideClicked(){
+        mRecyclerView.revertAnimation();
+    }
+
+}
