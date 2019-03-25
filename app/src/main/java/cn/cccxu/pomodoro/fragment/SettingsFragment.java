6e37ed5a609c0afaaae851ca8cdf5707a0544850
cccxu <cccxu@outlook.com>
2019-03-25 10:50:08 +0800

    add preference for all four settings

--- app/src/main/java/cn/cccxu/pomodoro/fragment/SettingsFragment.java
+++ app/src/main/java/cn/cccxu/pomodoro/fragment/SettingsFragment.java
@@ -16,6 +16,10 @@ import cn.cccxu.pomodoro.R;
 public class SettingsFragment extends Fragment {
 
     private MaterialSpinner tamato_spinner;
+    private MaterialSpinner shortrest_spinner;
+    private MaterialSpinner longrest_spinner;
+    private MaterialSpinner gap_spinner;
+
     private String lengthOfTamato;
     private String lengthOfShortRest;
     private String tamatoBeforeLongRest;
@@ -37,7 +41,38 @@ public class SettingsFragment extends Fragment {
         lengthOfLongRest = settings.getString("LengthOfLongRest", "15");
 
         tamato_spinner = (MaterialSpinner) view.findViewById(R.id.tamato_spinner);
+        shortrest_spinner = (MaterialSpinner) view.findViewById(R.id.shortrest_spinner);
+        longrest_spinner = (MaterialSpinner) view.findViewById(R.id.longrest_spinner);
+        gap_spinner = (MaterialSpinner) view.findViewById(R.id.gap_spinner);
+
         tamato_spinner.setItems("15", "25", "45");
+        shortrest_spinner.setItems("5", "10", "15");
+        longrest_spinner.setItems("10", "15", "25");
+        gap_spinner.setItems("3", "4", "6");
+
+        switch(tamatoBeforeLongRest){
+            case "3":
+                gap_spinner.setSelectedIndex(0);
+                break;
+            case "4":
+                gap_spinner.setSelectedIndex(1);
+                break;
+            case "6":
+                gap_spinner.setSelectedIndex(2);
+                break;
+        }
+
+        switch(lengthOfLongRest){
+            case "10":
+                longrest_spinner.setSelectedIndex(0);
+                break;
+            case "15":
+                longrest_spinner.setSelectedIndex(1);
+                break;
+            case "25":
+                longrest_spinner.setSelectedIndex(2);
+                break;
+        }
 
         switch(lengthOfTamato){
             case "15":
@@ -51,6 +86,44 @@ public class SettingsFragment extends Fragment {
                 break;
         }
 
+        switch(lengthOfShortRest){
+            case "5":
+                shortrest_spinner.setSelectedIndex(0);
+                break;
+            case "10":
+                shortrest_spinner.setSelectedIndex(1);
+                break;
+            case "15":
+                shortrest_spinner.setSelectedIndex(2);
+                break;
+        }
+
+        shortrest_spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(MaterialSpinner view, int position, long id, Object item) {
+                editor.putString("LengthOfShortRest", item.toString());
+                editor.commit();
+                //Toast.makeText(view.getContext(), settings.getString("LengthOfTamato", "25"), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        longrest_spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(MaterialSpinner view, int position, long id, Object item) {
+                editor.putString("LengthOfLongRest", item.toString());
+                editor.commit();
+                //Toast.makeText(view.getContext(), settings.getString("LengthOfTamato", "25"), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        gap_spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(MaterialSpinner view, int position, long id, Object item) {
+                editor.putString("TamatoBeforeLongRest", item.toString());
+                editor.commit();
+                //Toast.makeText(view.getContext(), settings.getString("LengthOfTamato", "25"), Toast.LENGTH_LONG).show();
+            }
+        });
 
         tamato_spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener() {
             @Override
@@ -60,6 +133,7 @@ public class SettingsFragment extends Fragment {
                 //Toast.makeText(view.getContext(), settings.getString("LengthOfTamato", "25"), Toast.LENGTH_LONG).show();
             }
         });
+
         return view;
     }
 
